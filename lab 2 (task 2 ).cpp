be2b753ba07652f1fb8250fe11ffa9d5740a789b
khatirjan294926-alt <khatirjan294926@gmail.com>
2026-02-24 00:12:00 +0500

    Create lab 2 (task 2 ).cpp

---
+++ lab 2 (task 2 ).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout <<"enter your name" 
+	<< endl;
+	cout <<"enter your registration number" 
+	<< endl;
+	cout <<"enter your CGPA" 
+	<< endl;
+	cout <<"enter your password"
+	<< endl;
+	cout <<"enter your pin"
+	<< endl;
+	cout <<"enter your password"
+	<< endl;
+	cout <<"dear customer the service are not available today. sorry for inconvenience created" 
+	<< endl;
+	cout <<"\" happy to help!\"..." << endl;
+	cout <<"\"great question!\"..." << endl;
+	cout <<"\"nice to meet you!\"..." << endl;
+	cout <<"\"may i ask why that is?\"..." << endl;
+	cout <<"\"thanks for bringing this to our attention!\"..." << endl;
+	cout <<"\"i completely understand why you'd want that\"..." << endl;
+	cout <<"\"thanks to gemini!\"..." << endl;
+	
+	              
+	return 0;
+}
